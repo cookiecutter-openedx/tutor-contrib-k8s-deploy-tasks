1c6156c5da0b9ca73514eb49335284f109a75068
lpm0073 <lpm0073@gmail.com>
2024-01-03 19:34:12 -0600

    chore: package for Quince

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     packages=find_namespace_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["tutor>=16.0.0,<17.0.0"],
+    install_requires=["tutor>=17.0.0,<18.0.0"],
     entry_points={"tutor.plugin.v1": ["k8s_deploy_tasks = tutork8s_deploy_tasks.plugin"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
